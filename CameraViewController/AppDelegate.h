18625af7d991a5dff58caf2b53ac42f15b0d94f2
sj442 <sj442@cornell.edu>
2015-01-29 15:04:34 -0500

    Initial Commit

---
+++ CameraViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CameraViewController
+//
+//  Created by Sunayna Jain on 1/29/15.
+//  Copyright (c) 2015 Enhatch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CameraViewController/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CameraViewController
+//
+//  Created by Sunayna Jain on 1/29/15.
+//  Copyright (c) 2015 Enhatch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
